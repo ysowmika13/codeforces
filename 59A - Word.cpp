b07ce5f7b5d9e4a15a3a978d3bc35b4fb630b2a4
sowmika <94032290+ysowmika13@users.noreply.github.com>
2023-08-10 20:41:03 +0530

    Create 59A - Word.cpp

---
+++ 59A - Word.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int countup=0,countlow=0,i;
+    for(i=0;i<s.size();i++)
+    {
+        if(isupper(s[i]))
+        {
+            countup++;
+        }
+        else
+        {
+            countlow++;
+        }
+    }
+    if(countup>countlow)
+    {
+        char ch;
+        
+        for(i=0;i<s.size();i++)
+        {
+            ch=toupper(s[i]);
+            cout<<ch;
+        }
+    }
+    else
+    {
+        char ch;
+        for(i=0;i<s.size();i++)
+        {
+            ch=tolower(s[i]);
+            cout<<ch;
+        }
+    }
+}
