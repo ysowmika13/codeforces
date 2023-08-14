6e716c9aa161a45ee605285fb51e3c48e30b2be7
sowmika <94032290+ysowmika13@users.noreply.github.com>
2023-08-14 19:59:27 +0530

    Create 110A - NearlyLuckyNumber.cpp

---
+++ 110A - NearlyLuckyNumber.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    int count=0;
+    cin>>n;
+    while(n)
+    {
+        if(n%10==4 || n%10==7)
+        {
+            count++;
+        }
+        n/=10;
+    }
+    if(count==4 || count==7)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
